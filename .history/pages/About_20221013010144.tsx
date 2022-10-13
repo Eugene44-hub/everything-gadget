6917b59cada7e6cbed82974f9ce7765ec219a4d4
Eugene-44hub <eugeneishado@gmail.com>
2022-10-13 01:05:35 +0100

    Sass setup

---
+++ .history/pages/About_20221013010144.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../styles/About.module.scss'
+const About = ():JSX.Element => {
+  return (
+    <div className=''>About</div>
+  )
+}
+
+export default About
